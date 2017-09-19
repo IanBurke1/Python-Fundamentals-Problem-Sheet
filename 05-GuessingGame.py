e18cf9500c9a1fb8535a33c3c8e88994e7c50333
Ian Burke <iano.burke@gmail.com>
2017-09-19 23:00:02 +0100

    list function

--- 05-GuessingGame.py
+++ 05-GuessingGame.py
@@ -7,7 +7,7 @@
 import random # import random package to use random function
 
 # random function generates a random number between 1 and 100 and store in num.
-num = random.randint(0,101) 
+num = random.randint(0, 101)
 
 print ("Guess a number between 1 and 100")
 
@@ -15,17 +15,17 @@ guessCount = 0 # counter to count the number of guesses
 
 # create a while loop for user to guess multiple times
 while True:
-    userGuess = input() # user's guess is stored as int in userGuess
-    userGuess = int(userGuess)
+    guess = input() # user's guess is stored as int in guess
+    guess = int(guess)
     guessCount += 1 # increment count by + 1 for every guess
 
-    if userGuess < num: #if user's guess is less than random num then..
+    if guess < num: #if user's guess is less than random num then..
         print("Too low")
 
-    elif userGuess > num: #if guess is bigger than number then..
+    elif guess > num: #if guess is bigger than number then..
         print("Too high")
 
-    elif userGuess == num: #user guess equals number then..
+    elif guess == num: #user guess equals number then..
         print("Winner winner chicken dinner!")
         print("Guesses taken: " , guessCount)
         break # end the while loop 
---
+++ 06-LargestSmallestList.py
@@ -0,0 +1,22 @@
+# Author: Ian Burke
+# Module: Emerging Technologies
+# Date: September, 2017
+
+# Problem Sheet: https://emerging-technologies.github.io/problems/python-fundamentals.html
+
+# create a function to determine largest & smallest elements in a list
+def func():
+    list = [] # initialising array list
+
+    print("Enter number of elements")
+    listnum = int(input()) # taking in user input as integer
+
+    # loop through the list
+    for i in range(listnum):
+        print("Enter element to list: ")
+        elements = int(input()) # taking in list elements
+        list.append(elements) # appending elements to list
+
+    print(list)
+
+func() # call function to run
