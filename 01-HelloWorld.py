45b7e2457d5c61923185733425574ac5fcf8f1a9
Ian Burke <iano.burke@gmail.com>
2017-09-19 15:13:30 +0100

    Hello World

---
+++ 01-HelloWorld.py
@@ -0,0 +1,7 @@
+# Author: Ian Burke
+# Module: Emerging Technologies
+# Date: September, 2017
+
+# Problem Sheet: https://emerging-technologies.github.io/problems/python-fundamentals.html
+
+print("Hello, world!")
