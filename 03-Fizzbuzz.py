0bd0a3de58fd77cf1df2c040f9575832ceb3df1d
Ian Burke <iano.burke@gmail.com>
2017-09-19 16:16:57 +0100

    fizzbuzz working

--- 03-Fizzbuzz.py
+++ 03-Fizzbuzz.py
@@ -6,9 +6,12 @@
 
 # loop from 1 to 100
 for i in range (1, 101):
-    if i % 3 == 0: # if the number is evenly divided by 3 with no remainder "== 0" then..
+    
+    if i % 3 == 0 and i % 5 ==0: # if the number is evenly divided by 3 with 0 remainder and same with 5 then..
+        print("fizzbuzz") #print out fizzbuzz
+    elif i % 3 == 0: # if the number is evenly divided by 3 with no remainder "== 0" then..
         print("fizz")
     elif i % 5 == 0: # else if the number is evenly divided by just 5 then
         print("buzz")
     else: # if not above then print the number 
-      print(i)
+        print(i)
