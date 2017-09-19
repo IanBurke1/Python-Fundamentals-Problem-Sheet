7cc2b551c167610747ba8bc5956baf08b6776c84
Ian Burke <iano.burke@gmail.com>
2017-09-19 23:15:24 +0100

    merge lists

---
+++ 08-MergeSortList.py
@@ -0,0 +1,18 @@
+# Author: Ian Burke
+# Module: Emerging Technologies
+# Date: September, 2017
+
+# Problem Sheet: https://emerging-technologies.github.io/problems/python-fundamentals.html
+
+# create a function to merge and sort lists
+def mergeSort():
+    # create two lists
+    list1 = [1,4,6]
+    list2 = [2,3,5]
+    
+    list1.extend(list2) # Merge list2 into list1
+    
+    print(list1)
+
+
+
