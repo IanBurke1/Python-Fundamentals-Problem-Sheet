f819c8124e02d1792492e48a90b84fd732cfbc31
Ian Burke <iano.burke@gmail.com>
2017-09-19 21:23:24 +0100

    factorial problem

---
+++ 04-FactorialDigitSum.py
@@ -0,0 +1,15 @@
+# Author: Ian Burke
+# Module: Emerging Technologies
+# Date: September, 2017
+
+# Problem Sheet: https://emerging-technologies.github.io/problems/python-fundamentals.html
+
+def factorial(n):
+    sum = 0
+    
+    for i in range (1,n + 1):
+        n *= n
+
+    
+
+factorial(10)
