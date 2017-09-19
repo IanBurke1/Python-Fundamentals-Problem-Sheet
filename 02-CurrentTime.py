4198792c0bbdba5977530a5f3249ceaac18a00bd
Ian Burke <iano.burke@gmail.com>
2017-09-19 15:29:15 +0100

    current time working

---
+++ 02-CurrentTime.py
@@ -0,0 +1,10 @@
+# Author: Ian Burke
+# Module: Emerging Technologies
+# Date: September, 2017
+
+# Problem Sheet: https://emerging-technologies.github.io/problems/python-fundamentals.html
+
+import time
+
+currentTime = time.asctime(time.localtime(time.time()))
+print("Current Time: ", currentTime)
