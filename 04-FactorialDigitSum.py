a4d8e2ca459e75a1cef4021279f575cda154fee6
Ian Burke <iano.burke@gmail.com>
2017-09-23 20:41:47 +0100

    factorial and sum working

--- 04-FactorialDigitSum.py
+++ 04-FactorialDigitSum.py
@@ -3,13 +3,20 @@
 # Date: September, 2017
 
 # Problem Sheet: https://emerging-technologies.github.io/problems/python-fundamentals.html
+
 import math
 
 def factorial(n):
     
-    num = math.factorial(n)
-    print(num)
+    fact = math.factorial(n) # importing a function that calculates the factorial of n
+    print("Factorial of ", n, "is: ", fact)
+
+    x = str(fact) # changing factorial from int to string
+    # Ref: https://stackoverflow.com/questions/6429638/how-to-split-a-string-into-integers-in-python
+    s = [int(i) for i in str(x)] # taking the string and putting it into an array while splitting it into integers using loop
+    print("Sum of digits in the number 100! is: ", sum(s)) # print out the sum of integers in array
+
 
     
 
-factorial(10)
+factorial(100) # call the function
