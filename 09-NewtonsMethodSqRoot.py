1409c2f5eb051705536f6119af94e09933aa0fd6
Ian Burke <iano.burke@gmail.com>
2017-09-28 15:31:25 +0100

    adding comments

--- 09-NewtonsMethodSqRoot.py
+++ 09-NewtonsMethodSqRoot.py
@@ -3,14 +3,16 @@
 # Date: September, 2017
 
 # Problem Sheet: https://emerging-technologies.github.io/problems/python-fundamentals.html
-import math
 
+import math # Importing math functions from python documentation
+
+# create a function to find the square root
 def sqrt():
     x = 100
-    ans = math.sqrt(x)
+    ans = math.sqrt(x) # using math.sqrt function to find square root of a number
     print("Square Root of ", x, " is: ", ans)
 
 
 
 
-sqrt()
+sqrt() # call function to run
